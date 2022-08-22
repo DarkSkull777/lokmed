8227d082b901bb3532e495d5310c5033de90b5d9
Dimas Ramadani <59839225+DarkSkull777@users.noreply.github.com>
2022-08-23 04:53:59 +0700

    Create adfin.php

---
+++ adfin.php
@@ -0,0 +1,53 @@
+<?php
+// Author Name : DarkSkull7
+// Information List : 276 List
+// contact here : hpdos075@gmail.com
+// Telegram : @XSkull7
+// greetz : BraveXploiter
+
+print "   
+	
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                         
+                Admin Finder CMS Lokomedia - by DarkSkull7
+  Information list : 276 List
+  Thanks to  : BraveXploiter
+";
+
+echo "masukan site  : ";
+$target = trim(fgets(STDIN));
+$list = "cakil_wordlist.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
+}
+
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
+
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\n\n [".date('H:m:s')."] Mencoba : $log => MANTAP!!\n";
+	}else{
+		print "\n[".date('H:m:s')."] Mencoba : $log => YAH NT..";
+	}
+}
+  
+?>
